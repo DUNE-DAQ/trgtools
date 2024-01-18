9a6c020a95fe05b7dfc4c5cee4322be906e03270
aeoranday <alejandro@oran.day>
2024-01-18 12:17:18 +0100

    Processes TPs and gives warnings.

--- python/trgtools/TAData.py
+++ python/trgtools/TAData.py
@@ -10,6 +10,11 @@ import daqdataformats
 import trgdataformats
 
 class TAData:
+    _FAIL_TEXT_COLOR = '\033[91m'
+    _WARNING_TEXT_COLOR = '\033[93m'
+    _BOLD_TEXT = '\033[1m'
+    _END_TEXT_COLOR = '\033[0m'
+
     ta_dt = np.dtype([
                       ('adc_integral', np.uint64),
                       ('adc_peak', np.uint64),
@@ -25,13 +30,36 @@ class TAData:
                       ('time_start', np.uint64),
                       ('type', np.uint8)
                      ])
+    tp_dt = np.dtype([
+                      ('adc_integral', np.uint32),
+                      ('adc_peak', np.uint32),
+                      ('algorithm', np.uint8),
+                      ('channel', np.int32),
+                      ('detid', np.uint16),
+                      ('flag', np.uint16),
+                      ('time_over_threshold', np.uint64),
+                      ('time_peak', np.uint64),
+                      ('time_start', np.uint64),
+                      ('type', np.uint8),
+                      ('version', np.uint16)
+                     ])
 
     def __init__(self, filename):
         self._h5_file = HDF5RawDataFile(filename)
         self._set_ta_frag_paths(self._h5_file.get_all_fragment_dataset_paths())
         self.ta_data = np.zeros((len(self._frag_paths),), dtype=self.ta_dt)
-        self.run_id = int(filename.split("_")[1][3:])
-        self.sub_run_id = int(filename.split("_")[2][:4])
+        self.tp_data = []
+        tmp_name = filename.split("run")[1]
+        try:
+            self.run_id = int(tmp_name.split("_")[0])
+        except:
+            print(self._WARNING_TEXT_COLOR + "WARNING: Couldn't find Run ID in file name. Using run id 0." + self._END_TEXT_COLOR)
+            self.run_id = 0
+        try:
+            self.sub_run_id = int(tmp_name.split("_")[1])
+        except:
+            print(self._WARNING_TEXT_COLOR + "WARNING: Couldn't find SubRun ID in file name. Using SubRun ID 1000." + self._END_TEXT_COLOR)
+            self.sub_run_id = 1000
 
     def _set_ta_frag_paths(self, frag_paths) -> None:
         """
@@ -45,12 +73,17 @@ class TAData:
     def get_ta_frag_paths(self) -> list:
         return self._frag_paths
 
-    def load_all_frags(self) -> None:
-        for idx, frag_path in enumerate(self._frag_paths):
-            ta = self._h5_file.get_frag(frag_path)
-            ta_datum = trgdataformats.TriggerActivity(ta.get_data())
-            old_entry = self.ta_data[idx].copy()
-            self.ta_data[idx] = np.array([(
+    def load_frag(self, frag_path, index=None) -> (np.ndarray, np.ndarray):
+        ta = self._h5_file.get_frag(frag_path)
+        if ta.get_data_size() == 0:
+            print(self._FAIL_TEXT_COLOR + self._BOLD_TEXT + "WARNING: Empty fragment. Returning empty array." + self._END_TEXT_COLOR)
+            print(self._WARNING_TEXT_COLOR + self._BOLD_TEXT + f"INFO: Fragment Path: {frag_path}" + self._END_TEXT_COLOR)
+            return np.zeros((0,), dtype=self.ta_dt), np.zeros((0,), dtype=self.tp_dt)
+        if index == None:
+            index = self._frag_paths.index(frag_path)
+
+        ta_datum = trgdataformats.TriggerActivity(ta.get_data())
+        np_ta_datum = np.array([(
                                 ta_datum.data.adc_integral,
                                 ta_datum.data.adc_peak,
                                 np.uint8(ta_datum.data.algorithm),
@@ -65,3 +98,33 @@ class TAData:
                                 ta_datum.data.time_start,
                                 np.uint8(ta_datum.data.type))],
                                 dtype=self.ta_dt)
+
+        num_tps = ta_datum.n_inputs()
+        np_tp_datum = np.zeros((num_tps,), dtype=self.tp_dt)
+        for idx, tp in enumerate(ta_datum):
+            np_tp_datum[idx] = np.array([(
+                                        tp.adc_integral,
+                                        tp.adc_peak,
+                                        tp.algorithm,
+                                        tp.channel,
+                                        tp.detid,
+                                        tp.flag,
+                                        tp.time_over_threshold,
+                                        tp.time_peak,
+                                        tp.time_start,
+                                        tp.type,
+                                        tp.version)],
+                                        dtype=self.tp_dt)
+        self.tp_data.append(np_tp_datum)
+        self.ta_data[index] = np_ta_datum
+
+        return np_ta_datum, np_tp_datum
+
+    def load_all_frags(self) -> None:
+        miscount = 0
+        for idx, frag_path in enumerate(self._frag_paths):
+            ta_datum, _ = self.load_frag(frag_path, idx)
+            if len(ta_datum) == 0:
+                miscount += 1
+        if miscount != 0:
+            print(self._FAIL_TEXT_COLOR + self._BOLD_TEXT + f"WARNING: Skipped {miscount} frags." + self._END_TEXT_COLOR)
