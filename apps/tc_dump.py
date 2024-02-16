1e0ab285b313052910c3a38f2d41090c95f99a03
aeoranday <alejandro@oran.day>
2024-02-16 12:49:21 +0100

    Added time spans and ADC integral plots.

--- apps/tc_dump.py
+++ apps/tc_dump.py
@@ -4,7 +4,6 @@ tpstream file.
 """
 
 import os
-import sys
 import argparse
 
 import numpy as np
@@ -82,6 +81,80 @@ def plot_pdf_histogram(
     pdf.savefig()
     plt.close()
 
+    return None
+
+
+def plot_pdf_scatter(
+        x_data: np.ndarray,
+        y_data: np.ndarray,
+        plot_details_dict: dict,
+        pdf: PdfPages) -> None:
+    """
+    Plot a scatter plot for the given x and y data to a PdfPages object.
+
+    Parameters:
+        x_data (np.ndarray): Array to use as x values.
+        y_data (np.ndarray): Array to use as y values.
+        plot_details_dict (dict): Dictionary with keys such as 'title', 'xlabel', etc.
+        pdf (PdfPages): The PdfPages object that this plot will be appended to.
+
+    Returns:
+        Nothing. Mutates :pdf: with the new plot.
+    """
+    # May or may not have a preferred style on the scatter, e.g., marker, color, size.
+    scatter_style = plot_details_dict.get('scatter_style', {})
+
+    plt.figure(figsize=(6, 4))
+    plt.scatter(x_data, y_data, **scatter_style)
+
+    # Asserts that the following need to be in the plotting details.
+    # Who wants unlabeled plots?
+    plt.title(plot_details_dict['title'])
+    plt.xlabel(plot_details_dict['xlabel'])
+    plt.ylabel(plot_details_dict['ylabel'])
+
+    plt.tight_layout()
+    pdf.savefig()
+    plt.close()
+
+    return None
+
+
+def plot_pdf_errorbar(
+        x_data: np.ndarray,
+        y_data: np.ndarray,
+        plot_details_dict: dict,
+        pdf: PdfPages) -> None:
+    """
+    Plot a scatter plot for the given x and y data to a PdfPages object.
+    Error bars are handled by :plot_details_dict: since they are a style
+    choice.
+
+    Parameters:
+        x_data (np.ndarray): Array to use as x values.
+        y_data (np.ndarray): Array to use as y values.
+        plot_details_dict (dict): Dictionary with keys such as 'title', 'xlabel', etc.
+        pdf (PdfPages): The PdfPages object that this plot will be appended to.
+
+    Returns:
+        Nothing. Mutates :pdf: with the new plot.
+    """
+    plt.figure(figsize=plot_details_dict.get('figsize', (6,4)))
+
+    errorbar_style = plot_details_dict.get('errorbar_style', {})
+    plt.errorbar(x_data, y_data, **errorbar_style)
+
+    # Asserts that the following need to be in the plotting details.
+    # Who wants unlabeled plots?
+    plt.title(plot_details_dict['title'])
+    plt.xlabel(plot_details_dict['xlabel'])
+    plt.ylabel(plot_details_dict['ylabel'])
+
+    plt.legend()
+
+    plt.tight_layout()
+    pdf.savefig()
+    plt.close()
     return None
 
 
@@ -215,7 +288,8 @@ def parse():
     parser.add_argument(
         "--end-frag",
         type=int,
-        help="Fragment index to stop processing (i.e. not inclusive). Takes negative indexing. Default: 0.",
+        help="Fragment index to stop processing (i.e. not inclusive)."
+             + "Takes negative indexing. Default: 0.",
         default=0
     )
     parser.add_argument(
@@ -380,15 +454,75 @@ def main():
                 min_time = np.min(time)  # Prefer making the relative time change.
                 plot_pdf_histogram(time - min_time, plot_dict[tc_key], pdf, linear, log)
                 if not no_anomaly:
-                    write_summary_stats(time - min_time, anomaly_filename, plot_dict[tc_key]['title'])
+                    write_summary_stats(time - min_time, anomaly_filename,
+                                        plot_dict[tc_key]['title'])
                 continue
 
             plot_pdf_histogram(data.tc_data[tc_key], plot_dict[tc_key], pdf, linear, log)
             if not no_anomaly:
-                write_summary_stats(data.tc_data[tc_key], anomaly_filename, plot_dict[tc_key]['title'])
+                write_summary_stats(data.tc_data[tc_key], anomaly_filename,
+                                    plot_dict[tc_key]['title'])
 
         # "Analysis" plots
+        # ==== Time Delta Comparisons =====
         plot_pdf_time_delta_histograms(data.tc_data, data.ta_data, pdf, time_label)
+
+        tc_adc_integrals = np.array([np.sum(tas['adc_integral']) for tas in data.ta_data])
+        adc_integrals_dict = {
+                'title': "TC ADC Integrals",
+                'xlabel': "ADC Integral"
+        }
+        plot_pdf_histogram(tc_adc_integrals, adc_integrals_dict, pdf, linear, log)
+        # =================================
+
+        # ==== ADC Integral vs Number of TAs ====
+        integral_vs_num_tas_dict = {
+                'title': "TC ADC Integral vs Number of TAs",
+                'xlabel': "Number of TAs",
+                'ylabel': "TC ADC Integral",
+                'scatter_style': {
+                    'alpha': 0.6,
+                    'c': 'k',
+                    's': 2
+                }
+        }
+        plot_pdf_scatter(data.tc_data['num_tas'], tc_adc_integrals, integral_vs_num_tas_dict, pdf)
+        # =======================================
+
+        # ==== Time Spans Per TC ====
+        time_candidate = data.tc_data['time_candidate']
+        time_end = data.tc_data['time_end']
+        time_start = data.tc_data['time_start']
+        tc_min_time = np.min((time_candidate, time_end, time_start))
+
+        time_candidate -= tc_min_time
+        time_end -= tc_min_time
+        time_start -= tc_min_time
+
+        if seconds:
+            tc_min_time = tc_min_time * TICK_TO_SEC_SCALE
+            time_candidate = time_candidate * TICK_TO_SEC_SCALE
+            time_end = time_end * TICK_TO_SEC_SCALE
+            time_start = time_start * TICK_TO_SEC_SCALE
+
+        yerr = np.array([time_candidate - time_start, time_end - time_candidate]).astype(np.int64)
+        time_spans_dict = {
+                'title': "TC Relative Time Spans",
+                'xlabel': "TC",
+                'ylabel': time_label,
+                'errorbar_style': {
+                    'yerr': yerr,
+                    'capsize': 4,
+                    'color': 'k',
+                    'ecolor': 'r',
+                    'label': f"Avg Ticks / TC: {(time_candidate[-1] - time_candidate[0]) / len(time_candidate):.2f}",
+                    'marker': '.',
+                    'markersize': 0.01
+                }
+        }
+        tc_count = np.arange(len(time_candidate))
+        plot_pdf_errorbar(tc_count, time_candidate, time_spans_dict, pdf)
+        # ===========================
 
     return None
 
