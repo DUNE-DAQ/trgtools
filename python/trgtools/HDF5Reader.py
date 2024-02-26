e703f27cb52f8bff3faf65a9ff1055642d0f0a61
aeoranday <alejandro@oran.day>
2024-02-26 16:49:46 +0100

    Made concrete some common methods.

--- python/trgtools/HDF5Reader.py
+++ python/trgtools/HDF5Reader.py
@@ -1,9 +1,10 @@
 """
 Generic HDF5Reader class to read and store data.
 """
+from hdf5libs import HDF5RawDataFile
+
 import abc
 
-from hdf5libs import HDF5RawDataFile
 
 class HDF5Reader(abc.ABC):
     """
@@ -19,6 +20,9 @@ class HDF5Reader(abc.ABC):
     _BOLD_TEXT = '\033[1m'
     _END_TEXT_COLOR = '\033[0m'
 
+    # Counts the number of empty fragments.
+    _num_empty = 0
+
     def __init__(self, filename: str, quiet: bool = False) -> None:
         """
         Loads a given HDF5 file.
@@ -30,12 +34,12 @@ class HDF5Reader(abc.ABC):
         # Generic loading
         self._h5_file = HDF5RawDataFile(filename)
         self._fragment_paths = self._h5_file.get_all_fragment_dataset_paths()
-        # Derived classes should filter fragment paths here.
-
         self.run_id = self._h5_file.get_int_attribute('run_number')
         self.file_index = self._h5_file.get_int_attribute('file_index')
 
         self._quiet = quiet
+
+        # Derived classes should filter the fragment paths after super().__init__.
 
         return None
 
@@ -49,17 +53,28 @@ class HDF5Reader(abc.ABC):
         """
         ...
 
-    @abc.abstractmethod
     def get_fragment_paths(self) -> list[str]:
         """ Return the list of fragment paths. """
-       ...
+        return list(self._fragment_paths)
 
     @abc.abstractmethod
     def read_fragment(self, fragment_path: str) -> None:
         """ Read one fragment from :fragment_path:. """
         ...
 
-    @abc.abstractmethod
     def read_all_fragments(self) -> None:
         """ Read all fragments. """
-        ...
+        for fragment_path in self._fragment_paths:
+            _ = self.read_fragment(fragment_path)
+
+        # self.read_fragment should increment self._num_empty.
+        # Print how many were empty as a debug.
+        if not self._quiet and self._num_empty != 0:
+            print(
+                    self._FAIL_TEXT_COLOR
+                    + self._BOLD_TEXT
+                    + f"WARNING: Skipped {self._num_empty} frags."
+                    + self._END_TEXT_COLOR
+            )
+
+        return None
