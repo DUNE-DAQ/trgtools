04db73c0d4955aad8ec1767d02c7ee50fc971b83
Alejandro Oranday <alejandro@oran.day>
2024-10-03 09:55:40 +0200

    Remove Unused Option.
    Users should define which maker/plugin in a config JSON.

--- apps/process_tpstream.cxx
+++ apps/process_tpstream.cxx
@@ -162,8 +162,6 @@ int main(int argc, char const *argv[])
   app.add_option("-o", output_file_path, "Output TPStream file path");
   std::string channel_map_name = "VDColdboxChannelMap";
   app.add_option("-m", channel_map_name, "Detector Channel Map");
-  std::string plugin_name = "TAMakerHorizontalMuonAlgorithm";
-  app.add_option("-p", plugin_name, "Trigger Activity plugin name.");
   std::string config_name;
   app.add_option("-j", config_name, "Trigger Activity and Candidate config JSON to use.")->required();
   uint64_t skip_rec(0);
